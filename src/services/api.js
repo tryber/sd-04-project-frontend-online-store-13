4f887c4b8e48e03776203223317763831afe8c2d
leonardoinatel <leonardodamasceno@gec.inatel.br>
2020-06-17 16:20:10 -0300

    Requisito 01 Done - Teste Okay

--- src/services/api.js
+++ src/services/api.js
@@ -1,22 +1,24 @@
 export async function getCategories() {
-  await fetch('https://api.mercadolibre.com/sites/MLB/categories')
-    .then(response => response.json())
+  return await fetch('https://api.mercadolibre.com/sites/MLB/categories')
+    .then(data => data.json())
     .catch(err => console.log('getCategoriesAPI', err))
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
   if (!categoryId && query) {
-    await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`)
-      .then(response => response.json())
+    return await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`)
+      .then(data => data.json())
       .catch(err => console.log('GetProductsFromQuery', err))
   }
   else if (!query && categoryId) {
-    await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`)
-      .then(response => response.json())
+    return await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`)
+      .then(data => data.json())
       .catch(err => console.log('GetProductsFromCategory', err))
   } else {
-      await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`)
-        .then(response => response.json())
+      return await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=
+        ${categoryId}&q=
+        ${query}`)
+        .then(data => data.json())
         .catch(err => console.log('getProductsFromCategoryAndQuery', err))
   }
 }  
