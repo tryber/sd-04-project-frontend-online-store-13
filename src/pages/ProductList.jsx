211544cc91f5471c739d3557e83eeb49a969f72b
Lucas Ribas <lucas@mundoribas.com.br>
2020-06-17 17:47:50 -0300

    Lets merge - part 3

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,28 +1,18 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
 
 class ProductList extends React.Component {
-  // consructor(props) {
-  //   super(props);
-  //   this.state = { products: '' };
-  // }
-
   render() {
-    // const { products } = this.state;
-    // if (products === '') {
-      return (
-        <>
-          <div>
-            <h3>Categorias:</h3>
-          </div>
-          <div data-testid="home-initial-message">
-            <h1>Digite algum termo de pesquisa ou escolha uma categoria.</h1>
-          </div>
-        </>
-      );
-    }
-    // return <h1>Refazer pois deu errado</h1>
-  // }
+    return (
+      <>
+        <div>
+          <h3>Categorias:</h3>
+        </div>
+        <div data-testid="home-initial-message">
+          <h1>Digite algum termo de pesquisa ou escolha uma categoria.</h1>
+        </div>
+      </>
+    );
+  }
 }
 
 export default ProductList;
