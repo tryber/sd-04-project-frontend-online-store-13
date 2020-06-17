1ca233e32e689f057ee7bb7e8d1304a552ce567c
leonardoinatel <leonardodamasceno@gec.inatel.br>
2020-06-17 16:09:36 -0300

    First requisito

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,23 @@
 export async function getCategories() {
-  // implement here
+  await fetch('https://api.mercadolibre.com/sites/MLB/categories')
+    .then(response => response.json())
+    .catch(err => console.log('getCategoriesAPI', err))
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
-  // implement here
-}
+  if (!categoryId && query) {
+    await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`)
+      .then(response => response.json())
+      .catch(err => console.log('GetProductsFromQuery', err))
+  }
+  else if (!query && categoryId) {
+    await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`)
+      .then(response => response.json())
+      .catch(err => console.log('GetProductsFromCategory', err))
+  } else {
+      await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`)
+        .then(response => response.json())
+        .catch(err => console.log('getProductsFromCategoryAndQuery', err))
+  }
+}  
+
