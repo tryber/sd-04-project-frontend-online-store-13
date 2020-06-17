45e0fead7ce2bb2ed2fc415efb307da0364e1f69
Lucas Ribas <lucas@mundoribas.com.br>
2020-06-17 17:36:24 -0300

    Read to merge.

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -2,27 +2,27 @@ import React from 'react';
 // import { Link } from 'react-router-dom';
 
 class ProductList extends React.Component {
-  consructor(props) {
-    super(props);
-    this.state = { products: '' };
-  }
+  // consructor(props) {
+  //   super(props);
+  //   this.state = { products: '' };
+  // }
 
   render() {
-    const { products } = this.state;
-    if (products === '') {
+    // const { products } = this.state;
+    // if (products === '') {
       return (
         <>
           <div>
             <h3>Categorias:</h3>
           </div>
           <div data-testid="home-initial-message">
-            <h1>Digite algum termo de pesquisa ou escolha uma categoria</h1>;
+            <h1>Digite algum termo de pesquisa ou escolha uma categoria.</h1>
           </div>
         </>
       );
     }
-    return <h1>Refazer pois deu errado</h1>
-  }
+    // return <h1>Refazer pois deu errado</h1>
+  // }
 }
 
 export default ProductList;
