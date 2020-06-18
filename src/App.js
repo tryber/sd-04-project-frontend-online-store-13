de0895da9d57495f273bc5c5029411018fc44078
Victor L <victor.lustosacp@gmail.com>
2020-06-18 14:47:29 -0300

    Erro no render de cartpage

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 // import something from 'somewere'
 // só pra commitar
-//hehe
+import {Route, BrowserRouter, Switch} from 'react-router-dom'
+
+import emptyPage from './pages/CartPage';
+import CartButton from './components/CartButton';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <CartButton />
+      <Switch>
+        <Route exact path="/cart" component={emptyPage} />
+      </Switch>
+    </BrowserRouter>
+      
   );
 }
 
---
+++ src/pages/CartPage.jsx
@@ -0,0 +1,13 @@
+import React, { Component }  from 'react';
+
+class emptyPage extends  Component {
+  render() {
+    return (
+    <div data-testid="shopping-cart-empty-message">
+      <h1>Seu carrinho está vazio</h1>
+    </div>
+  )}
+}
+ 
+
+export default emptyPage;
