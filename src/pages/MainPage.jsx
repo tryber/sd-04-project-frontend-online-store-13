3c5e18b75f4d617e6d94da7caa923c1a2bc6d290
leonardoinatel <leonardodamasceno@gec.inatel.br>
2020-06-18 17:42:01 -0300

    Requisito 05 - Main content e inicio Lista de Produdos

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,37 +1,44 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import { getProductsFromCategoryAndQuery } from '../services/api';
-import Search from '../components/Search';
+import { getProductsFromCategoryAndQuery } from "../services/api";
+import Search from "../components/Search";
 
 class ProductList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      searchInput: '',
+      searchInput: "",
       results: null,
       categoryId: null,
-
     };
     this.onHandleChange = this.onHandleChange.bind(this);
     this.updateResults = this.updateResults.bind(this);
   }
 
   onHandleChange(event) {
-    this.setState({ searchInput: event.target.value })
+    this.setState({ searchInput: event.target.value });
   }
 
   updateResults(categoryId, searchInput) {
-    getProductsFromCategoryAndQuery(categoryId, searchInput).then(({ results }) => {
-      this.setState({
-        results,
-      })
-    })
+    getProductsFromCategoryAndQuery(categoryId, searchInput).then(
+      ({ results }) => {
+        this.setState({
+          results,
+        });
+      }
+    );
   }
 
   render() {
+    const { searchInput, categoryId, results } = this.state;
     return (
       <div>
-
+        <Search
+          searchInput={searchInput}
+          categoryId={categoryId}
+          onHandleChange={this.onHandleChange}
+          updateResults={this.updateResults}
+        />
       </div>
     );
   }
