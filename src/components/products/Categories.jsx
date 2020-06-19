13bb8713b95d21b202b0a6578dbc57e61503cecd
Victor L <victor.lustosacp@gmail.com>
2020-06-19 15:00:50 -0300

    CC

--- src/components/products/Categories.jsx
+++ src/components/products/Categories.jsx
@@ -1,47 +1,28 @@
 import React, { Component } from 'react';
-import * as api from '../../services/api';
 
-function CategorieInput(elem, selectedOption, optionChange) {
+function CategorieInput(elem) {
   return (
-    <label htmlFor={elem.id} data-testid="category">
-      <input
-        type="radio"
-        id={elem.id}
-        name="categorie"
-        value={elem.name}
-        checked={selectedOption === elem.id}
-        onChange={optionChange}
+    <p>
+      <label htmlFor={elem.id} data-testid="category">
+        <input
+          type="radio"
+          id={elem.id}
+          name="categorie"
+          value={elem.name}
         />
         {elem.name}
-    </label>
+      </label>
+    </p>
   );
 }
 
 class Categories extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      selectedOption: '',
-      produto: [],
-    };
-    this.optionChange = this.optionChange.bind(this);
-  }
-
-  optionChange(event) {
-    console.log(event.target.id);
-    this.setState({
-      selectedOption: event.target.id,
-    });
-    return api.getProductsFromCategoryAndQuery(this.state.selectedOption, '').then((results) => this.setState({ produto: results }));
-  }
-
   render() {
     const { categories } = this.props;
-    const { selectedOption } = this.state;
     if (!categories) return <span>Loading...</span>;
     return (
       <div>
-        {categories.map((elem) => CategorieInput(elem, selectedOption, this.optionChange))}
+        {categories.map((elem) => CategorieInput(elem))}
       </div>
     );
   }
