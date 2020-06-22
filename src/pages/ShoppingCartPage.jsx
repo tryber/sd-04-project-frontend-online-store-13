5bbb30d6d6c32cc0beaa990edd590d6f61028450
Victor L <victor.lustosacp@gmail.com>
2020-06-22 15:23:52 -0300

    1a6

--- src/pages/ShoppingCartPage.jsx
+++ src/pages/ShoppingCartPage.jsx
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
-import CartButton from '../components/CartButton';
+
 
 class emptyPage extends Component {
   render() {
     return (
       <div>
-        <CartButton />
         <h1 data-testid="shopping-cart-empty-message">
           Seu carrinho está vazio
         </h1>
