e63c875f882f7b58adfa6f0ced234346ecd707bf
Victor L <victor.lustosacp@gmail.com>
2020-06-24 15:27:51 -0300

    retirando class e colocando fuction

--- src/components/AddCartButton.jsx
+++ src/components/AddCartButton.jsx
@@ -1,29 +1,23 @@
-/* eslint-disable react/jsx-props-no-spreading */
-import React, { Component } from 'react';
+import React from 'react';
 
-class AddCartButton extends Component {
-  constructor(props) {
-    super(props);
-    this.addSession = this.addSession.bind(this);
-  }
+function AddCartButton(props) {
+  const { product } = props;
 
-  addSession(product) {
+  const addSession = (product) => {
     if (!sessionStorage.carrinho) sessionStorage.carrinho = JSON.stringify([]);
     const carrinho = JSON.parse(sessionStorage.carrinho);
     const colocarList = [...carrinho, product];
     sessionStorage.carrinho = JSON.stringify(colocarList);
-  }
+  };
 
-  render() {
-    return (
-      <button
-        data-testid="product-add-to-cart"
-        onClick={() => this.addSession(this.props.product)}
-      >
-        add carrinho
-      </button>
-    );
-  }
+  return (
+    <button
+      data-testid="product-add-to-cart"
+      onClick={() => addSession(product)}
+    >
+      add carrinho
+    </button>
+  );
 }
 
 export default AddCartButton;
