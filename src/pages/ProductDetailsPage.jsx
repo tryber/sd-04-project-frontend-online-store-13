9ead80c3fd782829113ea4f3a864a9f08d51a35f
leonardoinatel <leonardodamasceno@gec.inatel.br>
2020-06-26 15:45:53 -0300

    CC refatorado

--- src/pages/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 import ProductDetailInfo from '../components/ProductDetailInfo';
 import Rating from '../components/Rating';
 import AddCartButton from '../components/AddCartButton';
-import CartButton from '../components/CartButton';
 
 class ProductDetailsPage extends React.Component {
   render() {
